78e616876326a79be2a539bfc397a7c138293cbf
Ayman Aamam <57199947+Ayman-aa@users.noreply.github.com>
2024-09-17 06:37:10 +0000

    unnecess

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -26,11 +26,5 @@ int main()
     for (int i = 0; i < numAnimals; ++i)
     {
         delete animals[i];
-    }
-
-    // Additional test for deep copy (optional, but good for verification)
-    Dog* dog1 = new Dog();
-    Dog* dog2 = new Dog(*dog1);
-    delete dog1;
-    delete dog2;   
+    } 
 }
