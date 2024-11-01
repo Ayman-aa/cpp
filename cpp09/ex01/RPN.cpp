8e54de33606defcce77f4f7145a88ececb5ebb34
Ayman Aamam <57199947+Ayman-aa@users.noreply.github.com>
2024-11-01 14:26:42 +0100

    sir nshuf

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -1,66 +1,118 @@
 #include "RPN.hpp"
 
-RPN::RPN(){
-}
-
-RPN::~RPN(){
-}
-
-RPN::RPN(const RPN& other){
-    this->numbers = other.numbers;
-}
-
-RPN& RPN::operator=(const RPN& other){
-    this->numbers = other.numbers;
+RPN::RPN() {}
+RPN::~RPN() {}
+RPN::RPN(const RPN &other) : numbers(other.numbers) {}
+RPN &RPN::operator=(const RPN &other)
+{
+    if (this != &other)
+        numbers = other.numbers;
     return *this;
 }
 
-double RPN::strToDouble(std::string test){
-    std::stringstream ss;
-    double var;
-    ss << test;
-    ss >> var;
-    return var;
+bool RPN::isOperator(const std::string &token) const
+{
+    return (token == "+" || token == "-" || token == "*" || token == "/");
 }
 
-bool RPN::NameChecker(std::string const &name){
-    if (name.find_first_not_of("0123456789") == std::string::npos)
-        return true;
-    std::cout <<">>|"<< name << "|<<" << std::endl;
-    return false;
-}
+bool RPN::isNumber(const std::string &token) const
+{
+    if(token.empty())
+        return false;
 
-
-double RPN::rpn(std::stringstream & tokens) {
-    std::string token;
-    while (tokens >> token) {
-        if (token == "+" || token == "-" || token == "*" || token == "/") {
-            if (numbers.size() < 2) {
-                throw std::runtime_error("Error :no left numbers to perform on");
-            }
-            int b = numbers.top();
-            numbers.pop();
-            int a = numbers.top();
-            numbers.pop();
-
-            if (token == "+")
-                numbers.push(a + b);
-            if (token == "-")
-                numbers.push(a - b);
-            if (token == "*")
-                numbers.push(a * b);
-            if (token == "/")
-                numbers.push(a / b);
-
-        } else {
-            if (NameChecker(token) && strToDouble(token) < 10)
-                    numbers.push(strToDouble(token));
-            else
-                throw std::runtime_error("Error : bad input");
-        }
+    std::string::const_iterator it;
+    for(it = token.begin(); it != token.end(); it++)
+    {
+        if(!std::isdigit(*it))
+            return false;
     }
-    if(numbers.size() != 1)
-        throw std::runtime_error("Error");
+    return true;
+}
+
+// convert string to int using std::istringstream which is a stream class to operate on strings
+int RPN::strToInt(const std::string &str) const
+{
+    std::istringstream ss(str);
+    int result;
+    ss >> result;
+
+    if(ss.fail() || !ss.eof())
+        throw RPN::RPNException();
+    
+    return result;
+}
+
+/*
+    * 1. pop the last two numbers from the stack
+    * 2. perform the operation
+    * 3. push the result back to the stack
+*/
+void RPN::performOperation(const std::string &operation)
+{
+    if (numbers.size() < 2)
+        throw RPN::RPNException();
+
+    int num2 = numbers.top();
+    numbers.pop();
+
+    int num1 = numbers.top();
+    numbers.pop();
+
+    int result;
+
+    if (operation == "+")
+        result = num1 + num2;
+    else if (operation == "-")
+        result = num1 - num2;
+    else if (operation == "*")
+        result = num1 * num2;
+    else if (operation == "/")
+    {
+        if (num2 == 0)
+            throw RPN::RPNException();
+        result = num1 / num2;
+    }
+    else
+        throw RPN::RPNException();
+    
+    numbers.push(result);
+}
+
+/*
+    * 1. read the expression token by token
+    * 2. if the token is a number, push it to the stack
+    * 3. if the token is an operator, perform the operation
+    * 4. if the token is neither a number nor an operator, throw an exception
+    * 5. if the stack has more than one number left, throw an exception
+    * 6. return the result
+*/
+int RPN::calculate(const std::string &expression)
+{
+    std::istringstream ss(expression);
+    std::string token;
+
+    while (!numbers.empty())
+        numbers.pop();
+
+    while (ss >> token)
+    {
+        if (isNumber(token))
+        {
+            int number = strToInt(token);
+            if (number >= 10)
+                throw RPN::RPNException();
+
+            numbers.push(number);
+        }
+        else if (isOperator(token))
+            performOperation(token);
+        else
+            throw RPN::RPNException();
+    }
+
+    if (numbers.size() != 1)
+        throw RPN::RPNException();
+    
     return numbers.top();
 }
 
--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -1,29 +1,31 @@
 #ifndef RPN_HPP
 #define RPN_HPP
-
 #include <iostream>
-#include <sstream>
 #include <string>
+#include <sstream>
+#include <exception>
 #include <stack>
+#include <cstdlib>
 
-using namespace std;
-
-
-class RPN {
+class RPN
+{
     private:
-        stack<double> numbers;
+        std::stack<int> numbers;
+        bool isOperator(const std::string &token) const;
+        bool isNumber(const std::string &token) const;
+        int strToInt(const std::string &str) const;
+        void performOperation(const std::string &operation);
     public:
         RPN();
+        RPN(const RPN &other);
+        RPN &operator=(const RPN &other);
         ~RPN();
-        RPN & operator=(RPN const & other);
-        RPN(const RPN & other);
-        double  rpn(std::stringstream & tokens);
-        bool NameChecker(std::string const &name);
-        double strToDouble(std::string test);
-
-        class RPNException: public std::exception {
-            public:
-                virtual const char* what() const throw() ;
-        };
+        int calculate(const std::string &expression);
+    
+    class RPNException : public std::exception
+    {
+        public:
+        virtual const char *what() const throw();
+    };
 };
 #endif
